0bf1cca689a9264b49e3c76e8ba73dc82a7e3919
Akshay kumar tyagi <79367523+akshaytyagi482@users.noreply.github.com>
2024-03-20 21:42:51 +0530

    completed

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
   });
+  const button = document.querySelector("button");
+const nav = document.querySelector(".lol")
+let isExpanded = button.getAttribute("aria-expanded") === "true";
+
+function open() {
+  button.setAttribute("aria-expanded", "true");
+  nav.style.top = "0";
+  nav.style.opacity = "100";
+  isExpanded = true;
+}
+
+function close() {
+  button.setAttribute("aria-expanded", "false");
+  nav.style.top = "-100%";
+  nav.style.opacity = "0";
+  isExpanded = false;
+}
+
+function toggle() {
+  isExpanded ? close() : open();
+}
+
+button.addEventListener("click", toggle);
